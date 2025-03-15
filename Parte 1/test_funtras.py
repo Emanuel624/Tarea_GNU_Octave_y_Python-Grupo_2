94d80575d32bb31b3d13e90ec559de9401e910fa
Randall-Bl <yitanr@estudiantec.cr>
2025-03-15 15:34:00 -0600

    Se implementa division en el print de funtra

--- Parte 1/test_funtras.py
+++ Parte 1/test_funtras.py
@@ -1,9 +1,16 @@
 import Pregunta1 as p
+# Códigos ANSI
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+RESET = "\033[0m"  # Restablece el color original
 
 raiz_3 = p.root_t(p.cos_t(3/7)+ p.ln_t(2), 3)
 sinh = p.sinh_t(p.sqrt_t(2))
 arctan = p.atan_t(p.exp_t(-1))
 
+print(f"{RED}Resultados del testFuntras{RESET}")
 print ((raiz_3*p.div_t(sinh))+arctan)
 
 
